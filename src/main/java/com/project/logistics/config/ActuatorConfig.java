16ca215bb09718f670302041e2ae0d740ac6f4e1
Alex <tomaalexandra8@gmail.com>
2024-05-29 10:23:49 +0300

    implement actuator

---
+++ src/main/java/com/project/logistics/config/ActuatorConfig.java
@@ -0,0 +1,24 @@
+package com.project.logistics.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class ActuatorConfig implements InfoContributor {
+
+private final CompanyInfo companyInfo;
+    @Override
+    public void contribute(Info.Builder builder) {
+        Map<String, String> userDetails = new HashMap<>();
+        userDetails.put("Current Date", companyInfo.getCurrentDate().toString());
+        userDetails.put("Company Profit", companyInfo.getCompanyProfit().toString());
+
+        builder.withDetail("Company Details", userDetails);
+    }
+}
