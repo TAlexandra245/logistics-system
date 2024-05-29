1a8a290ed5ea2fdcaa2c382b9ea01a4fc7d3304e
Alex <tomaalexandra8@gmail.com>
2024-05-29 11:33:33 +0300

    implement swagger for API documentation

--- src/main/java/com/project/logistics/controller/DestinationController.java
+++ src/main/java/com/project/logistics/controller/DestinationController.java
@@ -1,9 +1,15 @@
 package com.project.logistics.controller;
 
+import com.project.logistics.dao.Destination;
 import com.project.logistics.dto.DestinationDto;
 import com.project.logistics.exceptions.CanNotCreateEntity;
 import com.project.logistics.exceptions.ResourceNotFoundException;
 import com.project.logistics.service.DestinationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +29,15 @@ public class DestinationController {
         return destinationService.getAllDestinations();
     }
 
+    @Operation(summary = "Get a destination by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the destination",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Destination.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Destination not found",
+                    content = @Content)})
     @GetMapping("/{id}")
     public DestinationDto getDestination(@PathVariable Long id) throws ResourceNotFoundException {
         return destinationService.findById(id);
